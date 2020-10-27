4b2dadd3ea9959585e0fbfb07dacebb13fe4bf37
EasonLiu <bettercalleason@gmail.com>
2020-10-27 15:23:12 +0800

    add

---
+++ 程式碼/1027/8.練習5-美金轉新台幣-操作介面/index.js
@@ -0,0 +1,5 @@
+// 先統一得到目前的網頁上的要使用的DOM元素物件
+const moneyInput = document.getElementById("moneyInput");
+const usd2twd = document.getElementById("usd2twd");
+const twd2usd = document.getElementById("twd2usd");
+const moneyText = document.getElementById("moneyText");
