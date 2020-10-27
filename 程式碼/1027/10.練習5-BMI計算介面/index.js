2c7ceec23399d2baebfc23b74f907abce25c458f
Pull-Plus <55761750+Pull-Plus@users.noreply.github.com>
2020-10-27 16:03:44 +0800

    up & add

---
+++ 程式碼/1027/10.練習5-BMI計算介面/index.js
@@ -0,0 +1,5 @@
+// 先統一得到目前的網頁上的要使用的DOM元素物件
+const myHeight = document.getElementById("myHeight");
+const myWeight = document.getElementById("myWeight");
+const calc = document.getElementById("calc");
+const bmiText = document.getElementById("bmiText");
