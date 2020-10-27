63817716386d5c9c910930611c01f43dfcdb1d59
EasonLiu <bettercalleason@gmail.com>
2020-10-27 14:15:31 +0800

    add

---
+++ 程式碼/1027/4.計數器-文字與兩個按鈕/index.js
@@ -0,0 +1,10 @@
+const targetText = document.getElementById("target");
+const addButton = document.getElementById("add");
+const subButton = document.getElementById("sub");
+
+addButton.addEventListener("click", function () {
+  // 1. 得到目前網頁上的數字是多少
+  // 用一元正號(+)，先轉為數字再運算
+  // 2. 將目前的數字+1，然後呈現出來
+  targetText.innerHTML = +targetText.innerHTML + 1;
+});
