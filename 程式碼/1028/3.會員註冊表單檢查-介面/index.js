de5051800da36302792f0a41edf87792f095f271
Pull-Plus <55761750+Pull-Plus@users.noreply.github.com>
2020-10-28 13:57:36 +0800

    add

---
+++ 程式碼/1028/3.會員註冊表單檢查-介面/index.js
@@ -0,0 +1,6 @@
+const name = document.getElementById('name')
+const phone = document.getElementById('phone')
+const address = document.getElementById('address')
+const username = document.getElementById('username')
+const password = document.getElementById('password')
+const password2 = document.getElementById('password2')
