f5cb4652129cf042d129dd48bdae7dae79908c1f
Pull-Plus <55761750+Pull-Plus@users.noreply.github.com>
2020-10-28 15:33:48 +0800

    Update index.js

--- 程式碼/1028/7.生日下拉選單-for迴圈產生輸入選項/index.js
+++ 程式碼/1028/7.生日下拉選單-for迴圈產生輸入選項/index.js
@@ -3,6 +3,7 @@ const month = document.getElementById('month')
 const day = document.getElementById('day')
 
 let yearOptions = ''
+// 1910-2010
 for (let i = 1910; i < 2011; i++) {
   yearOptions += `<option value ="${i}">${i}</option>`
 }
@@ -10,6 +11,7 @@ for (let i = 1910; i < 2011; i++) {
 year.innerHTML = yearOptions
 
 let monthOptions = ''
+// 1-12
 for (let i = 1; i < 13; i++) {
   monthOptions += `<option value ="${i}">${i}</option>`
 }
@@ -17,6 +19,7 @@ for (let i = 1; i < 13; i++) {
 month.innerHTML = monthOptions
 
 let dayOptions = ''
+// 1-31
 for (let i = 1; i < 32; i++) {
   dayOptions += `<option value ="${i}">${i}</option>`
 }
