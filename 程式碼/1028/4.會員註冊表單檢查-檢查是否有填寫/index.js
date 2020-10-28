3de1b9ead578e816b364db807b17f97da43d8802
Pull-Plus <55761750+Pull-Plus@users.noreply.github.com>
2020-10-28 14:15:10 +0800

    Update index.js

--- 程式碼/1028/4.會員註冊表單檢查-檢查是否有填寫/index.js
+++ 程式碼/1028/4.會員註冊表單檢查-檢查是否有填寫/index.js
@@ -7,7 +7,8 @@ const password2 = document.getElementById('password2')
 const send = document.getElementById('send')
 
 send.addEventListener('click', function () {
-  //if(name.value === "")
+  //相當於 if(name.value === "")
+  //利用空白字串是假家族的情況來判斷
   if (!name.value) alert('姓名沒填')
 
   if (password.value !== password2.value) alert('兩次輸入的密碼不同')
