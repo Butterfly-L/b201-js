1eb53a1d2d21ebb306a2d8faab6580b828537fda
Pull-Plus <55761750+Pull-Plus@users.noreply.github.com>
2020-10-29 13:55:55 +0800

    Update index.js

--- 程式碼/1029/6.練習題-日曆-介面/index.js
+++ 程式碼/1029/6.練習題-日曆-介面/index.js
@@ -7,11 +7,15 @@ const now = new Date()
 //用樣版字串的寫法
 //yearAndMonth.innerHTML = `${now.getFullYear()}-${now.getMonth() + 1}`
 
+//用分開定義年和日的寫法
 const nowY = now.getFullYear()
 //注意回傳為0~11
 const nowM = now.getMonth() + 1
+
+// 呈現在網頁上
 yearAndMonth.innerHTML = nowY + '-' + nowM
 
+// 星期幾的陣列
 const weekList = ['日', '一', '二', '三', '四', '五', '六']
 
 let weekTitleDisplay = ''
@@ -20,4 +24,5 @@ for (let i = 0; i < weekList.length; i++) {
   weekTitleDisplay += `<th>${weekList[i]}</th>`
 }
 
+// 呈現在網頁上
 title.innerHTML = `<tr>${weekTitleDisplay}</tr>`
