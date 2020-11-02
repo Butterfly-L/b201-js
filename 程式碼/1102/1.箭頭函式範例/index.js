9526da5f4cb7a84c48ca632e542bb4f98d6ba805
Eddy Chang <eddy@joomla.com.tw>
2020-11-02 10:50:59 +0800

    Create index.js

---
+++ 程式碼/1102/1.箭頭函式範例/index.js
@@ -0,0 +1,24 @@
+const sum = function (a, b) {
+  return a + b
+}
+
+const sum2 = (a, b) => {
+  return a + b
+}
+
+// 只有在單行回傳時才能省略{}與return
+const sum3 = (a, b) => a + b
+
+//-------
+const increment = function (x) {
+  return x + 5
+}
+
+// 如果傳入參數只有一個，可以省略圓括號()
+const increment2 = x => x + 5
+
+
+//-------
+const func1 = (x) => x + 1
+// const func1 = (x) => { return x + 1 }
+const func2 = (x) => { x + 1 }
