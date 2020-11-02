cb30b5ecf9363be6af52b12f6c6f79f59e7f8444
Eddy Chang <eddy@joomla.com.tw>
2020-11-02 11:57:45 +0800

    Update index.js

--- 程式碼/1102/3.setInterval小時鐘範例/index.js
+++ 程式碼/1102/3.setInterval小時鐘範例/index.js
@@ -1,5 +1,7 @@
 const clock = document.getElementById('clock')
 
+// setInverval是每x微秒，執行一次回調函式中的程式碼
 setInterval(function () {
+  // 轉換為本地的(台灣時間字串
   clock.innerHTML = new Date().toLocaleTimeString()
 }, 1000)
