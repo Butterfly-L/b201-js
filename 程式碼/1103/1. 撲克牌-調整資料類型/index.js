ce8b687695c1579ff78b49df0be764bc2482b513
Pull-Plus <55761750+Pull-Plus@users.noreply.github.com>
2020-11-03 09:32:12 +0800

    add

--- 程式碼/1103/1. 撲克牌-調整資料類型/index.js
+++ 程式碼/1103/1. 撲克牌-調整資料類型/index.js
@@ -8,7 +8,6 @@ function createCard(suit, point) {
     suit: suit,
     point: point,
     display: function () {
-      console.log(suit, point)
       let displayPoint = point + ''
 
       if (this.point === 1) displayPoint = 'A'
@@ -19,13 +18,11 @@ function createCard(suit, point) {
       // poker函式庫產生卡片圖形的參數
       // 參考：https://tairraos.github.io/Poker.JS/#chinese-version-readme
 
-      console.log(suit, displayPoint)
-
       return Poker.getCardImage(100, this.suit, displayPoint)
     },
   }
 }
 
-const card = createCard('s', 11)
+const card = createCard('s', 13)
 console.log(card.display())
 desk.appendChild(card.display())
